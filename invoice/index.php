4056593e2e860721ce78fd92e24ee1d7000ab274
Basheer20212 <109953112+Basheer20212@users.noreply.github.com>
2023-03-05 00:05:39 +0530

    Add files via upload

---
+++ invoice/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style>
+        h1{
+  margin: 0%;
+  padding: 0%;
+}
+h2{
+  margin: 0%;
+  padding: 0%;
+}
+h3{
+  margin: 0%;
+  padding: 0%;
+}
+h4{
+  margin: 0%;
+  padding: 0%;
+}
+h5{
+  margin: 0%;
+  padding: 0%;
+}
+h6{
+  margin: 0%;
+  padding: 0%;
+} 
+h7{
+  margin: 0%;
+  padding: 0%;
+}
+p{
+  margin: 0%;
+  padding: 0%;
+}
+.container{
+  width: 90%;
+  /* 70% */
+  margin: auto;
+}
+.quantityNumber{
+  width: 30%;
+}
+.rateNumber{
+  width: 30%;
+}
+.amountNumber{
+  width: 30%;
+}
+
+.vdriveItSolutionUpper{
+  margin: 25px 0px 0px 0px ;
+}
+.vdriveItSolutionMiddle{
+  margin-top:25px ;
+  width: 50%;
+  float: left;
+}
+.vdriveItSolutionLast{
+  width: 50%;
+}
+.driveItSoultionImgDiv{
+  margin:20px auto 0px auto;
+  width: 45%;
+  float: left;
+}
+.driveItSoultionImgDiv img{
+  display: flex;
+  text-align: center;
+  justify-content: center;
+}
+.invoiceBillConvergeMiddle{
+  margin-top:25px;
+  width: 50%;
+  float: left;
+}
+.invoiceDateUpper{
+  margin:60px 0px 0px 0px;
+  float: right;
+  width: 40%;
+}
+.hr1Upper{
+  margin: 15px 0px 0px 0px;
+}
+.qtyRateAmountUpper{
+  margin-top: 30px;
+}
+.qtyRateAmountUpperMiddle{
+  background-color: #BFEAF5;
+  width: 100%;
+}
+.qtyRateAmountUpperLast{
+  text-align: center;
+  display: inline-flex;
+  margin-left: 50%;
+  width: 50%;
+}
+.venkateshUpper{
+  margin-top:15px ;
+}
+.venkateshMIddle{
+  margin-left:40px ;
+  float: left;
+  width: 45%;
+}
+.quantityRateAmountNumber{
+  text-align: center;
+  float: right;
+  display: flex;
+  width: 50%;
+}
+.hr2Upper{
+  margin: 75px 0px 0px 0px ;  
+}
+hr{
+  width: 100%;
+}
+.paymentUpper{
+  margin: 50px 0px 0px 0px ;
+  margin-left:50%; 
+}
+.paymentPayment{
+  float: left;
+  width: 30%;
+}
+.paymentNumber{
+  float: left;
+  width: 70%;
+  text-align: center;
+}
+.balanceDue{
+  margin-left:50%
+}
+.balanceDueH3{
+  margin-top: 5px;
+  float: left;
+  width: 30%;
+}
+.balanceDueH1{
+  float: left;
+  width: 70%;
+  text-align: center;
+}
+    </style>
+  </head>
+<body>
+  <div class="container">
+    <div class="vdriveItSolutionUpper">
+      <div class="vdriveItSolutionMiddle">
+        <div class="vdriveItSolutionLast" >
+          <h4>Vdrive IT Solution, Inc</h4>
+          <p> 800 E Campbell Rd, Suite 157
+            Richardson, TX 75081 US
+            972-559-3993
+            accounts@vdriveitsolutions.com
+            www.vdriveitsolutions.com</p>
+        </div>
+      </div>
+      <div class="driveItSoultionImgDiv">
+        <img src="driveitsolutions.png" alt="Drive It Solution Image">
+      </div>
+    </div>
+    <div class="invoiceBillConvergeUpper">
+        <div class="invoiceBillConvergeMiddle">
+          <h1>INVOICE</h1>
+          <h4>BILL TO</h4>
+          <h5>Converge Acquisition Inc</h5>
+        </div>
+        <div class="invoiceDateUpper">
+          <div style="margin: auto;width: 30%;float: left;">
+            <h4> <b>INVOICE #</b> </h4>
+            <h4> <b>DATE </b> </h4>
+            <h4> <b>DUE DATE </b> </h4>
+            <h4> <b>TERMS </b> </h4>
+          </div>
+          <div style="width: 30%;float: left;">
+            <p>4869</p>
+            <p>07/05/2022</p>
+            <p>08/04/2022</p>
+            <p>Net 30</p>
+          </div>
+      </div>
+    </div>
+    <div class="hr1Upper">
+      <div >
+        <hr style="width: 100%;">
+      </div>
+    </div>
+    <div class="qtyRateAmountUpper">
+      <div class="qtyRateAmountUpperMiddle">
+        <div class="qtyRateAmountUpperLast">
+          <h4 style="width: 30%;color: #1C315E;">QTY</h4>
+          <h4 style="width: 30%;color: #1C315E;">Rate</h4>
+          <h4 style="width: 30%;color: #1C315E;">Amount</h4>
+        </div>
+      </div>
+    </div>
+    <div class="venkateshUpper">
+      <div class="venkateshMIddle">
+        <h3 class="name">Venkatesh Thiruveedhula</h3>
+        <h5 class="date">Services for the month of June - 2022</h5> 
+      </div>
+      <div class="quantityRateAmountNumber">
+        <div class="quantityNumber"><h4>136      </h4></div>
+        <div class="rateNumber"    ><h4>75.00    </h4></div>
+        <div class="amountNumber"  ><h4>10,200.00</h4></div>
+      </div>
+    </div>
+    <div class="hr2Upper">
+      <div>
+        <hr>
+      </div>
+    </div>
+    <div class="paymentUpper">
+      <div>
+        <h3 class="paymentPayment" >PAYMENT</h3>
+        <h2 class="paymentNumber"  >10,200.00</h2>
+      </div>
+    </div>
+    <div  class="balanceDue">
+      <div>
+        <h3 class="balanceDueH3">BALANCE DUE</h3>
+        <h1 class="balanceDueH1"><b>$0</b></h1>
+      </div>
+    </div>
+  </div>  
+</body>
+</html>
